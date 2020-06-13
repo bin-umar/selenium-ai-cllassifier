5b1578f7a996fd90a36331109e3175d521fcfe5d
Abdullah <jakha_96-96@mail.ru>
2020-06-13 16:56:18 +0300

    Added icon scrapper script

---
+++ icon-scrapping/index.js
@@ -0,0 +1,60 @@
+const Scraper = require('images-scraper');
+const fs = require('fs');
+const path = require('path');
+const axios = require('axios');
+
+const google = new Scraper({
+  puppeteer: {
+    headless: false,
+  }
+});
+
+const fileName = path.resolve(__dirname, 'results.json');
+
+async function scrapeUrls () {
+  const results = await google.scrape('video camera white icon', 1000);
+  fs.writeFile(fileName, JSON.stringify(results, null, 4), () => null);
+}
+
+// scrapeUrls();
+
+async function download (url, path) {
+  const writer = fs.createWriteStream(path);
+
+  const response = await axios({
+    url,
+    method: 'GET',
+    responseType: 'stream'
+  });
+
+  response.data.pipe(writer);
+
+  return new Promise((resolve, reject) => {
+    writer.on('finish', resolve);
+    writer.on('error', reject);
+  });
+}
+
+const downloadImage = async () => {
+  const file = fs.readFileSync(fileName);
+  const list = JSON.parse(file);
+  let count = 0;
+
+  for (let i=0; i < list.length; i++) {
+    const { url } = list[i];
+    const imageExt = path.basename(url).split('.')[1];
+    const imageName = `${new Date().getTime()}.${imageExt}`;
+    const imagePath = path.resolve(__dirname, 'video_camera', imageName);
+
+    if (['jpg', 'png'].includes(imageExt)) {
+      try {
+        await download(url, imagePath);
+        console.log('downloaded image #: ' + (++count), url);
+      } catch (e) {
+        console.error('could\'nt download image ', url);
+      }
+    }
+  }
+};
+
+downloadImage();
