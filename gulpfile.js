cb0acf2ae1151c62645231d115a977fb0ac65b5e
Abdullah <jakha_96-96@mail.ru>
2020-06-18 19:19:57 +0300

    gulpfile edited

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,8 @@
-"use strict";
-
+'use strict';
 
 let gulp = require('gulp'),
-    boilerplate = require('appium-gulp-plugins').boilerplate.use(gulp);
+	boilerplate = require('appium-gulp-plugins').boilerplate.use(gulp);
 
 boilerplate({
-  build: 'test-ai-classifier-client',
-  transpileOut: 'build',
-  coverage: {
-    files: ['./test/unit/**/*-specs.js', '!./test/functional/**'],
-    verbose: true
-  },
+	transpileOut: 'build'
 });
